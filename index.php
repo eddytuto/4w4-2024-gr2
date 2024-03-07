1242d42ba27f1dbaf10f2a9e198f98c82d20ebf4
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-03-07 15:49:52 -0500

    scss du menu adaptatif, template-part vague et header.php

--- index.php
+++ index.php
@@ -1,25 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thème gr2</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/normalize.css';?>" >
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/style.css';?>" >
+<?php 
+/**
+ *  Modèle de base index.php 
+ */
+?>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<?php get_header(); ?>
 
-</head>
-<body>
-    <div id="menu" class="global">
-        <div class="entete__menu">
-            <?php wp_nav_menu(); ?>
-        </div>
-    </div>
-    <div id="entete" class="global">
-        <header class="entete__header">  
+<div id="entete" class="global">
+        <section class="entete__header">  
                         <!-- facebook -->
 
             <div class="entete__header__texte">    
@@ -28,13 +16,8 @@
                 <h3 class="bgc-text">TIM - Collège de Maisonneuve</h3>
             </div>   
             <div class="entete__header__button"><button class="entete__button">Événements</button></div>
-
-        </header>
-        <div class="vague">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" style="fill:var(--couleur-arriere-accueil)"></path>
-            </svg>
-        </div>
+        </section>
+<?php get_template_part("gabarit/vague"); ?>
     </div>
     <div id="accueil" class="global">
         <section class="accueil__section">
@@ -87,11 +70,7 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <a href="#">Lorem, ipsum.</a>  Minima velit qui unde odit quae, <a href="#">Lorem, ipsum.</a>  magni labore maiores facilis obcaecati dolore, ullam facere. Ducimus veniam reprehenderit, temporibus ab at possimus fugit?</p>
              <blockquote>Événement ipsum, dolor sit amet consectetur adipisicing elit. Accusantium a, repellat alias qui ut in ratione optio quia quae minus repudiandae ducimus aliquid aperiam unde atque tempore non. Non, magnam.</blockquote>
         </section>
-        <div class="vague">
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" style="fill:var(--couleur-arriere-footer)"></path>
-            </svg>
-        </div>
+        <?php get_template_part("gabarit/vague"); ?>
     </div>
     <div id="footer" class="global">
         <footer class="footer__section">
