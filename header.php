44e3ac1ed27e1b9932ba3618becdd5395352664b
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-03-14 16:35:48 -0400

    s6c2 ajout single.php, front-page.php, category.php

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 </head>
 <body>
     <div id="menu" class="global">
+        <?php echo get_custom_logo(); ?>
         <header class="entete__menu">
             <label id="burger" for="chk_burger">
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
