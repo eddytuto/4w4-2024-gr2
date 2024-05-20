c8b0b92e5447b13ae7553ff7b4a9ee946066f420
Eddy Martin <eddypmartin@gmail.com>
2024-05-20 19:06:10 -0400

    s14c2 categorie.php un gabarit pour afficher une carte de catégorie

--- front-page.php
+++ front-page.php
@@ -56,32 +56,10 @@ the_title() // echo du titre
         <section class="evenement__section">
             <h2>Les destinations par catégorie</h2>
             <article class="flexbox">
-            <?php
-            /*
-     get_categories() :  Est l'ensemble des catégorie du site
-     get_category() : Est l'ensemble des catégories d'un post
-
-            */ 
-              $categories = get_categories();
-              foreach ($categories as $elm_categorie){
-                $nom = $elm_categorie->name;
-                $id_categorie = $elm_categorie->term_id;
-                $description = wp_trim_words($elm_categorie->description, 10);
-                $nombre_destination = $elm_categorie->count;
-                $url_categorie = get_term_link($elm_categorie);
-                ?>
-                <div class="carte">
-                    <h3><?php echo $nom ?></h3>
-                    <p><?php echo $description ?></p>
-                    <p>id de la catégorie : <?php echo $id_categorie ?></p>
-                    <p>Nombre de destination : <?php echo $nombre_destination ?></p>
-                    <a href="<?php echo $url_categorie?>"><?php echo $nom ?> ... </a>
-              </div>
-             <?php } ?>
+            <?php get_template_part("gabarit/categorie"); ?>
              </article>
         </section>
         <?php get_template_part("gabarit/vague"); ?>
     </div>
-
     <?php  get_footer(); ?>
     
