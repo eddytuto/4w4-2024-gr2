7dc70ddedaffaac898e3e81a64f488af58e232cc
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-05-22 09:24:45 -0400

    s14c2 structure de menu amélioré

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
         echo get_custom_logo(); ?>
         <header class="entete__menu">
             <label id="burger" for="chk_burger">
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#000"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
+                <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" color="#fff"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16"></path></svg>
             </label>
             <input id="chk_burger" type="checkbox">
             <?php get_search_form(); ?>  
