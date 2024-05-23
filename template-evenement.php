693c27393b7c5169794b3bfa89f9ed745ebe5f82
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-05-23 16:09:56 -0400

    s15c1 modèle de page et champ personnalisé

--- template-evenement.php
+++ template-evenement.php
@@ -9,7 +9,11 @@
 <?php
 if ( have_posts() ) : the_post(); ?>
 <h1><?= get_the_title(); ?></h1>
-<?php the_content();?>
+<?php the_content();
+/* on peut aussi intégrer un champ personnalisé directement à l'intérieur d'une page ou d'un article en 
+utilisant la syntaxe [acf field="adresse"]
+[acf field="date"] et [acf field="heure"]
+?>
 <p>L'adresse de l'événement : <?php the_field('adresse'); ?></p>
 <p>La date de l'événement : <?php the_field('date'); ?></p> 
 <p>L'heure de l'événement : <?php the_field('heure'); ?></p> 
